5e1b358b48a514e64fc300f95763f0eb8c70167d
tech.sagarsh@gmail.com <tech.sagarsh@gmail.com>
2024-02-20 15:35:01 +0530

    unused code removed from the root page file

--- mace-wielders/app/page.tsx
+++ mace-wielders/app/page.tsx
@@ -24,16 +24,6 @@ export default function Home() {
   );
 }
 
-async function saveSubscription(subscription: any) {
-  // use axios & next syntax for api call
-  const response = await fetch("http://0.0.0.0:6500/save-service-worker", {
-    method: "post",
-    headers: { "Content-type": "application/json" },
-    body: JSON.stringify(subscription),
-  });
-  return response.json();
-}
-
 const requestNotificationPermissionGranted = async () => {
   const permission = await Notification.requestPermission();
 
@@ -41,18 +31,3 @@ const requestNotificationPermissionGranted = async () => {
     throw new Error("Notification permission not granted")
 }
 
-// Utility -
-function urlBase64ToUint8Array(base64String: any) {
-  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
-  const base64 = (base64String + padding)
-    .replace(/\-/g, "+")
-    .replace(/_/g, "/");
-
-  const rawData = window.atob(base64);
-  const outputArray = new Uint8Array(rawData.length);
-
-  for (let i = 0; i < rawData.length; ++i) {
-    outputArray[i] = rawData.charCodeAt(i);
-  }
-  return outputArray;
-}
